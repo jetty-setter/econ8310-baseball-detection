e8d5a0147dac2767d8f86210dc0bdfbae5062abf
Steph Simmons <ssimmons86@gmail.com>
2026-04-15 05:35:07 -0500

    Update directory paths in config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-XML_DIR    = "/Users/stephsimmons/Downloads/baseball_detection/econ8310-baseball-detection/XMLs"
-FRAMES_DIR = "/Users/stephsimmons/Downloads/baseball_detection/econ8310-baseball-detection/frames"
-OUTPUT_DIR = "/Users/stephsimmons/Downloads/baseball_yolo"
+XML_DIR    = "/Users/stephsimmons/baseball_detection/econ8310-baseball-detection/XMLs"
+FRAMES_DIR = "/Users/stephsimmons/baseball_detection/econ8310-baseball-detection/frames"
+OUTPUT_DIR = "/Users/stephsimmons/baseball_yolo"
 
 MODEL_SIZE  = "yolov8s.pt"
 EPOCHS      = 150
